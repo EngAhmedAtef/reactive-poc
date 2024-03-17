5b8b810f9d5d727a7256158c9039f2a51bef938e
Ahmed Atef <ahmed.abdelmaksoud@GizaSystems.onmicrosoft.com>
2024-03-17 03:22:25 +0200

    minor change

--- user-management/src/main/java/com/gizasystems/usermanagement/UserManagementApplication.java
+++ user-management/src/main/java/com/gizasystems/usermanagement/UserManagementApplication.java
@@ -1,9 +1,12 @@
 package com.gizasystems.usermanagement;
 
+import com.gizasystems.cssdb.annotation.ImportCssDb;
 import com.gizasystems.usermanagement.controller.UserController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+
+@ImportCssDb
 
 @SpringBootApplication
 public class UserManagementApplication {
